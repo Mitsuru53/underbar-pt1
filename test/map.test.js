0fe8484bcb11629d2933fdf1cfd0502c643a23cb
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-01-26 21:25:24 +0900

    add tests to map.test.js

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,20 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+  it('roop array length times', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const mappedArr = _.map(arr, (el) => el * el);
+    expect(mappedArr.length).toEqual(arr.length);
+  });
+  it('given string argument returns array of characters', () => {
+    const word = ['t', 'e', 's', 't'];
+    const mappedArr = _.map(word, (char) => char);
+    expect(mappedArr).toEqual(['t', 'e', 's', 't']);
+    expect(mappedArr.length).toEqual(word.length);
+  });
+  it('roop string length times', () => {
+    const word = ['t', 'e', 's', 't'];
+    const mappedArr = _.map(word, (char) => char);
+    expect(mappedArr.length).toEqual(word.length)
+  });
 });
