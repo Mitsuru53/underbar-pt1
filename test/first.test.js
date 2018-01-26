372e1d171e917c14e2299438c25813c3b101d67d
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-01-26 19:57:40 +0900

    add tests to first.test.js

--- test/first.test.js
+++ test/first.test.js
@@ -12,4 +12,16 @@ describe('first()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('two dimention and n = 1, returns 1 dimention array', () => {
+    expect(_.first([['a', 'b', 'c'], ['d', 'e', 'f']])).toEqual(['a','b','c']);
+  });
+
+  it('two dimention and n=  2, returns two dimention array', () => {
+    expect(_.first([['a', 'b', 'c'], ['d', 'e', 'f']],2)).toEqual([['a', 'b', 'c'], ['d', 'e', 'f']]);
+  });
+
+  it('two dimention and n > arrya length, returns two dimention array', () => {
+    expect(_.first([['a', 'b', 'c'], ['d', 'e', 'f']],5)).toEqual([['a', 'b', 'c'], ['d', 'e', 'f']]);
+  });
 });
