83d530430706e9d337ee58af6afbdbc77901b400
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-01-29 21:45:24 +0900

    add tests to uniq.test.js

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,21 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+  it('de-dups a obj', () => {
+    const obj = {
+      name: 'wai',
+      name: 'wai1',
+      age: 27,
+      sex: 'man'
+    };
+    expect(_.uniq(obj)).toEqual(['wai1', 27, 'man']);
+  });
+  it('returns empty array if given empty array', () => {
+    const emptyArray = [];
+    expect(_.uniq(emptyArray)).toEqual([]);
+  });
+  it('returns empty array if given empty obj', () => {
+    const emptyArray = {};
+    expect(_.uniq(emptyArray)).toEqual([]);
+  });
 });
