e30b2dec5c3a41549dc8db2060bcead5b2339088
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-01-29 20:59:44 +0900

    add tests to reduce.test.js

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -6,11 +6,29 @@ describe('reduce()', () => {
     const result = _.reduce(nums, (acc, num) => acc + num, 0);
     expect(result).toEqual(15);
   });
-
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
     const nums = [1, 2, 3, 4, 5];
     const result = _.reduce(nums, (a, b) => a + b);
     expect(result).toEqual(15);
   });
-
+  it('reduces an array of negative numbers to a sum, *with* an explicit initial value for the accumulator', () => {
+    const negativeNums = [-1, -2, -3, -4, -5];
+    const result = _.reduce(negativeNums, (a, b) => a + b, 0);
+    expect(result).toEqual(-15);
+  });
+  it('reduces an array of negative numbers to a sum, *without* an explicit initial value for the accumulator', () => {
+    const negativeNums = [-1, -2, -3, -4, -5];
+    const result = _.reduce(negativeNums, (a, b) => a + b);
+    expect(result).toEqual(-15);
+  });
+  it('reduces an empty array, *with* an explicit initial value for the accumulator', () => {
+    const emptyArray = [];
+    const result = _.reduce(emptyArray, (a, b) => a + b, 0);
+    expect(result).toEqual(0);
+  });
+  it('reduces an empty array, *without* an explicit initial value for the accumulator', () => {
+    const emptyArray = [];
+    const result = _.reduce(emptyArray, (a, b) => a + b);
+    expect(result).toEqual(undefined);
+  });
 });
